95e95e3d0737d5bd878b41627fa1d980f6028ae4
Dmitrii <dmitryabashkin@yandex.ru>
2022-08-27 18:41:16 +0300

    My first HW

--- homework.py
+++ homework.py
@@ -1,10 +1,10 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     def __init__(self, training_type: str,
-    duration: float,
-    distance: float,
-    speed: float,
-    calories: float)-> None:    
+                duration: float,
+                distance: float,
+                speed: float,
+                calories: float) -> None:    
     
         self.training_type = training_type
         self.duration = duration
@@ -23,26 +23,27 @@ class InfoMessage:
 class Training:
     """Базовый класс тренировки."""
 
-    LEN_STEP=0.65
-    M_IN_KM=1000
+    LEN_STEP = 0.65
+    M_IN_KM = 1000
     def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float,
-                 ) -> None:
-        self.action=action
-        self.duration=duration
-        self.weight=weight
+                action: int,
+                duration: float,
+                weight: float,
+                ) -> None:
+        self.action = action
+        self.duration = duration
+        self.weight = weight
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        dist=self.action * self.LEN_STEP / self.M_IN_KM 
-        #в км
+        dist = self.action * self.LEN_STEP / self.M_IN_KM 
+# в км
         return dist
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        mspeed=self.get_distance()/self.duration #dist or self.distance()???
+        mspeed = self.get_distance() / self.duration 
+#dist or self.distance()???
         return mspeed
 
     def get_spent_calories(self) -> float:
@@ -64,20 +65,21 @@ class Running(Training):
 
     coeff_calorie_1 = 18
     coeff_calorie_2 = 20
-    min_in_hour=60
+    min_in_hour = 60
 
     def __init__(self, 
-    action:int,
-    duration:float,
-    weight:float):
-    
-    #get_spent_calories()) методы не наследуются
+                action: int,
+                duration: float,
+                weight: float):
+
+#get_spent_calories()) методы не наследуются
         super().__init__(action, duration, weight)
 
-    def get_spent_calories(self)-> float:
+    def get_spent_calories(self) -> float:
         """ Расчет калорий"""
-        cal_Run=((self.coeff_calorie_1*self.get_mean_speed()-self.coeff_calorie_2)*self.weight/self.M_IN_KM*self.duration*self.min_in_hour)
+        cal_Run = ((self.coeff_calorie_1 * self.get_mean_speed() - self.coeff_calorie_2) * self.weight / self.M_IN_KM * self.duration * self.min_in_hour)
         return cal_Run
+
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
@@ -85,51 +87,53 @@ class SportsWalking(Training):
     coeff_calorie_3 = 0.035
     coeff_calorie_4 = 2
     coeff_calorie_5 = 0.029
-    MIN_IN_HOUR=60
+    MIN_IN_HOUR = 60
 
-    def __init__(self,action:int,
-    duration:float,
-    weight:float,
-    height:float):
-    #get_distance,get_mean_speed,show_training_info,get_spent_calories):
-        self.height=height 
-        super().__init__(action,duration,weight)
-    #,get_distance(),get_mean_speed(),show_training_info())
+    def __init__(self, 
+                action: int,
+                duration: float,
+                weight: float,
+                height: float):
+#get_distance,get_mean_speed,show_training_info,get_spent_calories):
+        self.height = height 
+        super().__init__(action, duration, weight)
+#,get_distance(),get_mean_speed(),show_training_info())
 
-    def get_spent_calories(self)-> float:
+    def get_spent_calories(self) -> float:
         """ Расчет калорий"""
-        cal_walk=(self. coeff_calorie_3 * self.weight + (self.get_mean_speed()**self. coeff_calorie_4 // self.height) * self. coeff_calorie_5 * self.weight) * self.duration*self.MIN_IN_HOUR 
+        cal_walk = (self. coeff_calorie_3 * self.weight + (self.get_mean_speed()**self. coeff_calorie_4 // self.height) * self. coeff_calorie_5 * self.weight) * self.duration*self.MIN_IN_HOUR 
         return cal_walk
+
 
 class Swimming(Training):
     """Тренировка: плавание."""
     coeff_calorie_6 = 1.1
     coeff_calorie_7 = 2
     LEN_STEP = 1.38
-    M_IN_KM=1000
+    M_IN_KM = 1000
 
     def __init__(self,
-    action:int,
-    duration:float,
-    weight:float,
-    length_pool:float,
-    count_pool:int):
-        self.length_pool=length_pool
-        self.count_pool=count_pool
-    #get_distance(),get_mean_speed(),show_training_info(),get_spent_calories()):
+                action:int,
+                duration:float,
+                weight:float,
+                length_pool:float,
+                count_pool:int):
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+#get_distance(),get_mean_speed(),show_training_info(),get_spent_calories()):
         super().__init__(action, duration, weight)
-    #get_distance(),get_mean_speed(),show_training_info())
+#get_distance(),get_mean_speed(),show_training_info())
 
-    def get_mean_speed(self) ->float:
-        mswim=self.length_pool*self.count_pool/self.M_IN_KM/self.duration
+    def get_mean_speed(self) -> float:
+        mswim = self.length_pool * self.count_pool / self.M_IN_KM / self.duration
         return mswim
 
-    def get_spent_calories(self)->float:
-        cal_Swim=(self.get_mean_speed()+self.coeff_calorie_6)*self.coeff_calorie_7*self.weight 
+    def get_spent_calories(self) -> float:
+        cal_Swim = (self.get_mean_speed() + self.coeff_calorie_6) * self.coeff_calorie_7 * self.weight 
         return cal_Swim
 
-    def get_distance(self)->float:
-        sw_distance=self.action*self.LEN_STEP/self.M_IN_KM
+    def get_distance(self) -> float:
+        sw_distance = self.action * self.LEN_STEP / self.M_IN_KM
         return sw_distance
 
 def read_package(workout_type: str, data: list) -> Training:
@@ -161,4 +165,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-
